738a11ed5cf0b370fca46150e18c089d97a87193
debusr <debusr@spearhead.home>
2025-05-28 14:33:22 +0200

    Initial creation of the basics NO LSP NO nvim-cmp

---
+++ lua/plugins/mini.lua
@@ -0,0 +1,83 @@
+return {
+    { "echasnovski/mini.nvim", version = false },
+    {
+        "echasnovski/mini.pairs",
+        event = "VeryLazy",
+        opts = {
+            modes = { insert = true, command = true, terminal = false },
+            -- skip autopair when next character is one of these
+            skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+            -- skip autopair when the cursor is inside these treesitter nodes
+            skip_ts = { "string" },
+            -- skip autopair when next character is closing pair
+            -- and there are more closing pairs than opening pairs
+            skip_unbalanced = true,
+            -- better deal with markdown code blocks
+            markdown = true,
+        },
+    },
+    {
+        'echasnovski/mini.files',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        config = function()
+            local MiniFiles = require("mini.files")
+            MiniFiles.setup({
+                mappings = {
+                    go_in = "<CR>", -- Map both Enter and L to enter directories or open files
+                    go_in_plus = "L",
+                    go_out = "-",
+                    go_out_plus = "H",
+                },
+            })
+            vim.keymap.set("n", "<leader>e", "<cmd>lua MiniFiles.open()<CR>", { desc = "Toggle mini file explorer" }) -- toggle file explorer
+            vim.keymap.set("n", "<leader>xf", function()
+                MiniFiles.open(vim.api.nvim_buf_get_name(0), false)
+                MiniFiles.reveal_cwd()
+            end, { desc = "Toggle into currently opened file" })
+        end,
+    },
+    {
+        "echasnovski/mini.trailspace",
+        event = { "BufReadPost", "BufNewFile" },
+        config = function()
+            local miniTrailspace = require("mini.trailspace")
+
+            miniTrailspace.setup({
+                only_in_normal_buffers = true,
+            })
+            vim.keymap.set("n", "<leader>cw", function() miniTrailspace.trim() end, { desc = "Erase Whitespace" })
+
+            -- Ensure highlight never reappears by removing it on CursorMoved
+            vim.api.nvim_create_autocmd("CursorMoved", {
+                pattern = "*",
+                callback = function()
+                    require("mini.trailspace").unhighlight()
+                end,
+            })
+        end,
+    },
+    {
+        'echasnovski/mini.comment',
+        version = false,
+        dependencies = {
+            "JoosepAlviste/nvim-ts-context-commentstring",
+        },
+        config = function()
+            -- disable the autocommand from ts-context-commentstring
+            require('ts_context_commentstring').setup {
+                enable_autocmd = false,
+            }
+
+            require("mini.comment").setup {
+                -- tsx, jsx, html , svelte comment support
+                options = {
+                    custom_commentstring = function()
+                        return require('ts_context_commentstring.internal').calculate_commentstring({ key =
+                            'commentstring' })
+                            or vim.bo.commentstring
+                    end,
+                },
+            }
+        end
+    },
+}
